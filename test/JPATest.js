923379c860da09e0d8218f5b4c09dd7d87f74a1d
evlach <evangelos.vlachogiannis@gmail.com>
2015-09-27 18:46:45 +0200

    refactoring to more JPA approach

--- test/JPATest.js
+++ test/JPATest.js
@@ -3,22 +3,12 @@ let assert = require("assert");
 let Foo = require("../examples/Foo");
 let m = require("..");
 let Persistence = require("./Persistence");
+let fooMetaModel = require("../examples/FooMetaModel");
 describe("cassandra-persistence", function ()
 {
     describe("#JPATest", function ()
     {
         let entityManager;
-        let fooMetaModel = new MetaModel({
-            keySpace: "imergo_test",
-            name: "foo",
-            fields: new Map([["id", "timeuuid"], ["name", "text"], ["created", "timeuuid"],
-                ["entity", "text"], ["entities", "list<text>"], ["simpleObjects", "list<text>"],
-                ["enabled", "boolean"]]),
-            partitionKeys: ["id"],
-            clusteringColumns: new Map([["name", "ASC"]]),
-            secondaryIndexes: ["name"],
-            entityClass: Foo
-        });
         it("should initiate JPA", function ()
         {
             let emFactory = Persistence.createEntityManagerFactory("Foo", config);
