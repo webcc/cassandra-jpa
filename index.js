98dc5636763fc1c87b0d917e7beb7cf1e2406695
evlach <evangelos.vlachogiannis@gmail.com>
2015-10-03 00:16:02 +0200

    CriteriaBuilder approach

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 "use strict";
 module.exports = {
     MetaModel: require("./lib/MetaModel"),
-    Criterion: require("./lib/Criterion"),
-    CriterionType: require("./lib/CriterionType"),
     JPAConfiguration: require("./lib/JPAConfiguration"),
-    QueryBuilder: require("./lib/QueryBuilder"),
+    CriteriaBuilder: require("./lib/CriteriaBuilder"),
     PersistenceUtils: require("./lib/PersistenceUtils"),
     Persistence: require("./lib/Persistence"),
     EntityManager: require("./lib/EntityManager"),
