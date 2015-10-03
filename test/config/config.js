67f5b6ae2d67875e5ed42581c708716e567a4c12
evangelos.vlachogiannis <evangelos.vlachogiannis@fit.fraunhofer.de>
2015-10-04 00:32:31 +0200

    refine for v0.4.1

--- test/config/config.js
+++ test/config/config.js
@@ -1,5 +1,5 @@
 "use strict";
-let jpa = require('cassandra-jpa');
+let jpa = require('../..');
 let configuration = null;
 module.exports = configuration || initConfig();
 function initConfig()
@@ -7,7 +7,7 @@ function initConfig()
     configuration = new jpa.JPAConfiguration();
     configuration.cassandra.contactPoints = ["localhost"];
     configuration.cassandra.keyspace = "tests";
-    configuration.logQueryObject = true;
+    configuration.logQueryObject = process.env.JPA_LOG || false;
     configuration.cassandra.contactPoints = [process.env.JPA_HOST || configuration.cassandra.contactPoints[0]];
     configuration.cassandra.keyspace = process.env.JPA_KEYSPACE || configuration.cassandra.keyspace;
     return configuration;
