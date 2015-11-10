cfd52769d4e77b8826f6e46af6f807136bfff843
Evangelos Vlachogiannis <evangelos.vlachogiannis@gmail.com>
2015-11-10 10:43:58 +0100

    fix(PersistenceUtils): make static calls in Factory and Persistence

--- test/EntityManagerFactory.test.js
+++ test/EntityManagerFactory.test.js
@@ -1,17 +1,18 @@
 "use strict";
 let assert = require("assert");
+let cassandra = require('cassandra-driver');
 let m = require("..");
-describe("cassandra-persistence", function ()
+describe("cassandra-jpa::EntityManagerFactory", function ()
 {
-    describe("#EntityManagerFactoryTest", function ()
+    it("should initiate EntityManagerFactory", function ()
     {
-        it("should initiate EntityManagerFactory", function ()
-        {
-            let config = require("./config/config.js");
-            config.cassandra.contactPoints = [ process.env.DBHOST || config.cassandra.contactPoints[0] ];
-            let emFactory = m.Persistence.createEntityManagerFactory("Foo", config);
-            let entityManager = emFactory.createEntityManager();
-            assert.equal(entityManager instanceof m.EntityManager, true);
-        });
+        let id = cassandra.types.TimeUuid.now();
+
+        let config = require("./config/config.js");
+        config.cassandra.contactPoints = [process.env.DBHOST || config.cassandra.contactPoints[0]];
+        let emFactory = m.Persistence.createEntityManagerFactory("Foo", config);
+        let entityManager = emFactory.createEntityManager();
+        assert.equal(entityManager instanceof m.EntityManager, true);
+        assert(m.Persistence.getPersistenceUnitUtils().toTimeUuid(id) instanceof cassandra.types.TimeUuid);
     });
 });
