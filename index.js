61cbc92552ca822544da841ad6d179780c784c63
darthcav <darth.cav@gmail.com>
2017-11-20 19:43:50 +0100

    remove test dep

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ module.exports = {
     EntityManagerFactory: require("./lib/EntityManagerFactory"),
     Entity: require("./lib/Entity"),
     CassandraClientFactory: require("./lib/CassandraClientFactory"),
-    err: require("./lib/JPAErrors"),
+    err: require("./lib/JPAErrors")/*,
     tests: {
         Foo: require("./test/helpers/Foo"),
         FooMetaModel: require("./test/helpers/FooMetaModel"),
         ExtendedFooMetaModel: require("./test/helpers/ExtendedFooMetaModel"),
         EntityManagerTest: require("./test/helpers/EntityManagerTest"),
         PureMetaModel:  require("./test/helpers/PureMetaModel")
-    }
+    }*/
 };
